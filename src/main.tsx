487bdd452d495e999b363351ce79b64694011d0a
Kyungbae Min <kbmin1129@gmail.com>
2025-07-27 14:29:54 +0900

    feat: defined react-dom router

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import { enableMocking } from "./server/brower.ts";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from '@/App.tsx';
+import { enableMocking } from '@/server/brower.ts';
+import './index.css';
 
 enableMocking({
-	serviceWorker: {
-		url: "/mockServiceWorker.js",
-	},
-	onUnhandledRequest: "bypass",
+  serviceWorker: {
+    url: '/mockServiceWorker.js',
+  },
+  onUnhandledRequest: 'bypass',
 }).then(() => {
-	createRoot(document.getElementById("root")!).render(
-		<StrictMode>
-			<App />
-		</StrictMode>,
-	);
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
 });
