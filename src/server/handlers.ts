14fee76ecbd76fc1dd98b021004bc13f02a18b17
JUCHEOLJIN <toto006521@gmail.com>
2025-07-29 11:43:46 +0900

    feat: 프로덕트 리스트 api

--- src/server/handlers.ts
+++ src/server/handlers.ts
@@ -1,17 +1,21 @@
 import { HttpResponse, http } from 'msw';
+import { products } from './data';
 
 const ERROR_CHANCE = 0.33;
 
 export const handlers = [
-  // test
   http.get('/api/product/list', async () => {
+    const isError = isErrorRandomly(ERROR_CHANCE);
+    if (isError) {
+      return new HttpResponse(null, {
+        status: 500,
+        statusText: 'Internal Server Error',
+      });
+    }
+
     return HttpResponse.json({
-      product: Array.from({ length: 10 }, (_, index) => ({
-        id: index + 1,
-        name: `Product ${index + 1}`,
-        price: 10000,
-        image: `https://via.placeholder.com/150?text=Product+${index + 1}`,
-      })),
+      products: products,
+      status: 200,
     });
   }),
 ];
@@ -20,12 +24,3 @@ function isErrorRandomly(threshold: number) {
   const randomNumber = Math.random();
   return randomNumber < threshold;
 }
-
-//상품
-// - 카테고리
-// - 수량
-// - 금액
-// - 설명
-// - 이미지
-// - 평점
-// - 좋아요(위시리스트)
